b31a199f0c19fdc1817ffa595ace7f5238fe108d
ezio84 <brabus84@gmail.com>
2017-05-11 00:20:50 -0400

    Oneplus3 : Enable real time lockscreen charging info
    Change-Id: I6b6bdcf904453c0195a0d96e8c5cc67f9a2ddaed

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -125,6 +125,9 @@ USE_DEVICE_SPECIFIC_CAMERA := true
 BOARD_CHARGER_ENABLE_SUSPEND := true
 BOARD_CHARGER_DISABLE_INIT_BLANK := true
 
+# Enable real time lockscreen charging current values
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
+
 # CM Hardware
 BOARD_HARDWARE_CLASS += $(PLATFORM_PATH)/cmhw
 TARGET_TAP_TO_WAKE_NODE := "/proc/touchpanel/double_tap_enable"
