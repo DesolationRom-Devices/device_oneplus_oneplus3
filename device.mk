f7466b9f60188dfe6c05cc2403ecd1e67c62ce61
REV3NT3CH <rev3nt3ch@gmail.com>
2017-07-16 22:01:04 -0700

    Enable & Properly Name Doze

--- device.mk
+++ device.mk
@@ -189,6 +189,10 @@ PRODUCT_PACKAGES += \
     qdcm_calib_data_samsung_s6e3fa5_1080p_cmd_mode_dsi_panel.xml \
     qdcm_calib_data_samsung_s6e3fa5r9s_1080p_cmd_mode_dsi_panel.xml
 
+# Doze mode
+PRODUCT_PACKAGES += \
+    OneplusDoze
+
 # Fingerprint sensor
 PRODUCT_PACKAGES += \
     fingerprintd \
